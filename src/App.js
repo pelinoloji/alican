aadbd41e99d733f68b3dd17c65794216ab8375ed
pelin <kayhanpelin@gmail.com>
2019-09-18 19:10:05 +0100

    add contact page layout

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./Navigation";
 import Contact from "./Contact";
-import Works from "./Works";
-
+import Works from "./Works/Works";
 import "./App.css";
+
 const App = () => {
   return (
     <Router>
       <div className="container">
         <Navigation />
         <Switch>
+          <Route exact path="/" component={Works} />
           <Route path="/contact" component={Contact} />
-          <Route path="/ui-ux" component={Works} />
         </Switch>
       </div>
     </Router>
--- src/Contact.js
+++ src/Contact.js
@@ -3,38 +3,42 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className="Contact col-8">
-      <div className="ContactImage col-3">BOX</div>
-      <div className="col-6">
-        <div className="NameTitle">Alican Hicyorulmaz</div>
-        <div>
+    <div className="Contact col-12">
+      <div className="ContactImage col-4"></div>
+      <div className="ContactDetails col-6">
+        <div className="NameTitle"><strong>Ali Can Hiçyorulmaz</strong></div>
+        <div className="FormText">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ut
           natus optio adipisci explicabo modi distinctio voluptatem ad nihil
-          beatae, quis perspiciatis deserunt quae alias quisquam! Sit, nam
-          vitae. Molestiae.
+          beatae, quis perspiciatis deserunt quae alias quisquam! <br />
+          <span>+90 530 300 60 77</span> | <span>hi@ali-can.co</span>
         </div>
-        <span>+90 530 300 60 77</span> | <span>hi@ali-can.co</span>
-      </div>
-      <div className="ContactForm">
-        <form>
-          <label>
-            First Name
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Last Name
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Email Adress
-            <input type="email" name="name" />
-          </label>
-          <label>
-            Message
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+        <div className="ContactForm">
+          <form>
+          <div>
+            <label>
+              First Name *
+            </label>
+            <input className="col-4" type="text" name="name" />
+
+            <label>
+              Last Name *
+            </label>
+            <input className="col-4" type="text" name="lastname" />
+          </div>
+            <label>
+              Email Address *
+            </label>
+            <input type="email" name="email" />
+
+            <label>
+              Message *
+            </label>
+            <input className="msgBox" type="text" name="message" />
+
+            <input className="button col-4" type="submit" value="Send" />
+          </form>
+        </div>
       </div>
     </div>
   );
